69221c7c1f6f95e9ba48682fa6acd88e4135e834
sasithhansaka <sasithhansaka285@gmail.com>
2025-03-27 20:47:11 +0530

    Add FeatureData module with images, titles, and content for features

---
+++ Frontend/src/data/FeatureData.jsx
@@ -0,0 +1,20 @@
+export const images = [
+    './src/images/Flag.png',
+    './src/images/Dollar.png',
+    './src/images/Blur.png',
+    './src/images/Alarm Clock.png',
+  ];
+  
+  export const titles = [
+    'Feature Flag',
+    'Monthly Fees',
+    'Smart IoT Integration',
+    'Real Time Monitoring',
+  ];
+  
+  export const content = [
+    'Owners Can Monitor Energy Usage Remotely, Improving Decision-Making And Operational Efficiency.',
+    'One Brand Name Card And Unlimited Various Things. Enjoy All The Day With Us.',
+    'Owners Can Monitor Energy Usage Remotely, Improving Decision-Making And Operational Efficiency.',
+    'Get instant updates on energy usage, allowing for quicker responses and better time management.',
+  ];
