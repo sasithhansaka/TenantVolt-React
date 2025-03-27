70f04ef0f0dac96d895df7bc4c2a1eac55ab3cab
sasithhansaka <sasithhansaka285@gmail.com>
2025-03-27 20:47:20 +0530

    Refactor FeaturesContainer to accept images, titles, and content as props

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Navbar from '../components/HomePage_Components/Navbar.jsx'
 import FeaturesContainer from '../components/HomePage_Components/FeaturesContainer.jsx'
 import MobileApp_Container from '../components/HomePage_Components/MobileApp_Container.jsx'
+import { images, titles, content } from '../data/FeatureData.jsx';
+
 
 function Home() {
+
+ 
   return (
     <div>
       <Navbar/>
-      <FeaturesContainer/>
+      <FeaturesContainer 
+       images={images}
+       titles={titles}
+       content={content}
+      />
       <MobileApp_Container/>
     </div>
   )
