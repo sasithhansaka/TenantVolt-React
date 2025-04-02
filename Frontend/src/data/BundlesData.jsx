40531813ff62c7ee05759430812a7a6348f9e15c
sasithhansaka <sasithhansaka285@gmail.com>
2025-04-02 23:29:29 +0530

    Refactor BundlesData structure for improved readability and consistency

--- Frontend/src/data/BundlesData.jsx
+++ Frontend/src/data/BundlesData.jsx
@@ -1,26 +1,26 @@
-export  const Bundles = [
-    {
-        id:"1", 
-        title: "1 PACK",
-        shipping: "Free Shipping",
-        price: "LKR.2000.00",
-        discountText: "STANDARD PRICE",
-        discountAmount: "+KR.2300.00"
-    },
-    {
-        id:"2",
-        title: "2 PACK",
-        shipping: "Free Shipping",
-        price: "LKR.3500.00",
-        discountText: "YOU HAVE 1/OK OFF",
-        discountAmount: "+KR.2000.00"
-    },
-    {
-        id:"3",
-        title: "3 PACK",
-        shipping: "Free Shipping",
-        price: "LKR.4500.00",
-        discountText: "YOU HAVE 1/OK OFF",
-        discountAmount: "+KR.2000.00"
-    }
+export const Bundles = [
+  {
+    id: "1",
+    title: "1 PACK",
+    shipping: "Free Shipping",
+    price: "LKR.2000.00",
+    discountText: "STANDARD PRICE",
+    discountAmount: "+KR.2300.00",
+  },
+  {
+    id: "2",
+    title: "2 PACK",
+    shipping: "Free Shipping",
+    price: "LKR.3500.00",
+    discountText: "YOU HAVE 1/OK OFF",
+    discountAmount: "+KR.2000.00"
+  },
+  {
+    id: "3",
+    title: "3 PACK",
+    shipping: "Free Shipping",
+    price: "LKR.4500.00",
+    discountText: "YOU HAVE 1/OK OFF",
+    discountAmount: "+KR.2000.00"
+  },
 ];
