1da1ae2e698f436d89facfdaf0beb53efe0a72c4
sasithhansaka <sasithhansaka285@gmail.com>
2025-04-03 09:37:06 +0530

    Add Dashboard component with tab navigation for improved user experience

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import DashboardNavbar from "../components/Dashboard/DashboardNavbar";
+// import DashboardHome from "../components/Dashboard/Home";
+import Billing from "../components/Dashboard/Billing";
+import Details from "../components/Dashboard/Details";
+import DashboardHome from "../components/Dashboard/Home";
+
+function Dashboard() {
+  const [activeTab, setActiveTab] = useState("Home");
+
+  const renderContent = () => {
+    switch(activeTab) {
+      case 'Home':
+        return <DashboardHome/>;
+      case 'Dashboard':
+        return <Details />;
+      case 'Billing':
+        return <Billing />;
+      default:
+        return <DashboardHome />;
+    }
+  };
+
+  return (
+    <div>
+      <DashboardNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div className="dashboard-content">
+        {renderContent()}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
