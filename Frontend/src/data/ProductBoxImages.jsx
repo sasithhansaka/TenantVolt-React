9ca9d45bc7e4791079294862f1ba1659739109fb
sasithhansaka <sasithhansaka285@gmail.com>
2025-04-03 14:58:22 +0530

    Add product images and create ProductBoxImages data file

---
+++ Frontend/src/data/ProductBoxImages.jsx
@@ -0,0 +1,7 @@
+export const images = [
+  // './src/images/Product Image1.png',
+  './src/images/Product Image1.png',
+  './src/images/Product Image2.png',
+  './src/images/Product Image4.png',
+  './src/images/Product Image3.png',
+];
