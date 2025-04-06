1adf0254db9a8685f5fd44a0ab30bde670c953ec
sasithhansaka <sasithhansaka285@gmail.com>
2025-04-06 16:22:59 +0530

    Refactor Dashboard Details and Electricity Usage components
    - Updated Details component to include ElectricityUsage component and removed unnecessary elements.
    - Modified Details.module.css for improved styling and layout adjustments.
    - Enhanced ElectricityUsage component with user data handling, chart rendering, and animations using Framer Motion.
    - Improved CSS styles in ElectricityUsage.module.css for better responsiveness and visual consistency.
    - Added tenant selection functionality and updated UI elements for better user experience.

--- Frontend/src/components/Dashboard/Details.jsx
+++ Frontend/src/components/Dashboard/Details.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Details.module.css'; // Assuming you have a CSS module for styling
+import ElectricityUsage from './ElectricityUsage';
 
 function Details() {
   return (
-    <div className={styles.details}>
-      <h2>Dashboard Details</h2>
-      <p>Detailed dashboard information goes here</p>
+    <div >
+      {/* <h2>Dashboard </h2> */}
+      <p></p>
+      <ElectricityUsage/>
     </div>
   );
 }
